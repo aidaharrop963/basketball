653eebf02618393c4f52fc4b07486c8b4dc1e626
sporty963 <ms.harrop963@gmail.com>
2015-12-10 22:08:56 +0000

    cookies

--- basketball.js
+++ basketball.js
@@ -1,3 +1,10 @@
-var promptForPlayerName = function (playerNumber) {
-    var name = prompt(playerNumber + " what is your name?");
+var promptForPlayerName = function (playerNumber1) {
+    var name = prompt(playerNumber1 + " what is your name?");
+    return name; 
+};
+var tryTwoPointShote = function () {
+    var Math.random()};
+    if (Math.random() > 0.2);
+    return true;
+    else return false; 
 };
