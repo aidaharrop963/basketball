07987711e00d4beea35a273f1ea441a7188fd686
sporty963 <ms.harrop963@gmail.com>
2015-12-17 21:33:55 +0000

    finished basktball.js

--- basketball.js
+++ basketball.js
@@ -2,9 +2,57 @@ var promptForPlayerName = function (playerNumber1) {
     var name = prompt(playerNumber1 + " what is your name?");
     return name; 
 };
-var tryTwoPointShote = function () {
-    var Math.random()};
-    if (Math.random() > 0.2);
+var tryTwoPointShot = function () {
+    var randOne = Math.random();
+    var randTwo = Math.random(); 
+   if (randOne > 0.2 &&	randTwo > 0.2) { 
     return true;
-    else return false; 
-};
+   }else {
+    return false; 
+   }
+  
+
+}
+
+var tryThreePointShot = function() {
+    var randOne = Math.random();
+    var randTwo = Math.random();
+     if (randOne <= 0.2 || randTwo >= 0.8){ 
+         return true; 
+     } 
+     else {
+         return false; 
+     }
+
+      
+    }
+
+var getShotString = function(playerName,points,wasSuccessful) {
+    if (wasSuccessful === true) { 
+        return playerName + "attempted a " + points + " pointer. It was good";
+    }
+        else{
+         return playerName = "attempted a " + points + " pointer. It was not good"; 
+        }
+}
+
+var updateScore = function(wasShotMade,currentScore,points) { 
+    if (wasShotMade === true){ 
+        return currentScore + points; 
+        }
+    else{
+        return currentScore;
+    }
+}
+
+var isEndOfGame = function (currentScore,playerName){
+    if (currentScore >= 20){
+    alert("The game is over " + playerName + " won"); 
+    return true; 
+    }
+    else{ 
+    return false; 
+}
+} 
+
+    
